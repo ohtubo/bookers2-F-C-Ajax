a9cff841c23b346fed263315c548e90ee329cb7f
ohtubo <brte14020@gmail.com>
2021-07-09 07:08:13 +0000

    [Start]_bookers2-F-C-Ajax

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,29 @@
+class BookCommentsController < ApplicationController
+
+  def create
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      redirect_to book_path(@book)
+    else
+      @newbook = Book.new
+      @user = @book.user
+      @book_comment = BookComment.new
+      render template: "books/show"
+    end
+
+  end
+
+  def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+  resources :books
+  resources :books, only: [:new, :create, :index, :show, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+end
