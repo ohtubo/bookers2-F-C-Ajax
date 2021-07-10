6525f00f0de09e8c86e35a8e60c23c7814e2f978
ohtubo <brte14020@gmail.com>
2021-07-10 07:26:08 +0000

    [Finish]_bookers2-F-C-Ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,22 +2,18 @@ class BookCommentsController < ApplicationController
 
   def create
     @book = Book.find(params[:book_id])
-    @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
-    if @comment.save
-      redirect_to book_path(@book)
-    else
-      @newbook = Book.new
-      @user = @book.user
-      @book_comment = BookComment.new
-      render template: "books/show"
-    end
-
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    @book_comment.save
   end
 
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
+    book_comment.destroy
+    # BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    # redirect_to book_path(params[:book_id])
   end
 
   private
